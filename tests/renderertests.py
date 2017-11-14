fb6d5b9de0fce1335f4ea380a9f034c781cd7dd1
Nathan Johnson <nwjohnson@gmail.com>
2017-11-13 21:02:31 -0600

    Removed Background class and ScrollBufferOld class.

--- tests/renderertests.py
+++ tests/renderertests.py
@@ -17,34 +17,9 @@ class TestBackgroundBuffer(unittest.TestCase):
         self.game_map.load(map_data)
         self.game_map.load_attr_map(attr_data)
         self.game_map.load_mapmap(b'\x00\x02\x00\x01\x00\x01')
-        self.background = renderer.Background(self.game_map)
 
     def test_map(self):
         self.assertEqual(self.game_map.map_width, 2)
         self.assertEqual(self.game_map.map_height, 1)
         self.assertEqual(len(self.game_map.sections), 2)
         self.assertEqual(len(self.game_map.attr_sections), 2)
-
-    def test_scroll(self):
-        second_to_last_col = []
-        last_col = []
-        x, _ = self.background.offset
-        for row_data in self.background.rows:
-            tile_number, attr = row_data[x + map.Map.section_width - 1]
-            second_to_last_col.append(tile_number)
-            tile_number, attr = row_data[x + map.Map.section_width]
-            last_col.append(tile_number)
-        self.assertEqual(second_to_last_col, [0]*renderer.Background.height)
-        self.assertEqual(last_col, [0]*renderer.Background.height)
-        self.background.scroll(1, 0)
-        second_to_last_col = []
-        last_col = []
-        x, _ = self.background.offset
-        for row_data in self.background.rows:
-            tile_number, attr = row_data[x + map.Map.section_width - 1]
-            second_to_last_col.append(tile_number)
-            tile_number, attr = row_data[x + map.Map.section_width]
-            last_col.append(tile_number)
-        self.assertEqual(second_to_last_col, [0]*renderer.Background.height)
-        self.assertEqual(last_col, [1]*map.Map.section_height + [0]*map.Map.section_height)
-        
